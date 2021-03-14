f0f8df6e2642984575ccd89913a6d33f60fee389
Anna Myzika <myzikaanya@gmail.com>
2021-03-15 02:16:30 +0300

    добавлено решение

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
 import galleryItems from "./gallery-items.js";
-console.log(galleryItems);
+
+const listGalleryRef = document.querySelector('.js-gallery');
+const modalRef = document.querySelector('.js-lightbox');
+const imgRef = document.querySelector('.lightbox__image');
+const btnRef = document.querySelector('.lightbox__button');
+const overlayRef = document.querySelector('.lightbox__overlay');
 
 
+const markup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li><a class="gallery__link" href='${original}'><img class='gallery__image' src='${preview}' data-source= '${original}' alt='${description}'></a></li>`
+);
+listGalleryRef.insertAdjacentHTML('beforeend', markup.join(''));
 
-{/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>    
-</li> */}
+listGalleryRef.addEventListener('click', onModalClick);
+
+let activeIndex = 0;
+
+function onModalClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName === 'IMG') {
+    modalRef.classList.add('is-open');
+    imgRef.src = e.target.dataset.source;
+  }
+
+  for (let elem of markup){
+    if (elem.includes(e.target.src)) {
+      activeIndex = markup.indexOf(elem)
+    }
+  }
+};
+
+btnRef.onclick = closeModal;
+function closeModal() {
+modalRef.classList.remove('is-open');
+  imgRef.removeAttribute('src');
+};
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeModal()
+  }
+});
+
+overlayRef.addEventListener('click', closeModal);
+
+window.addEventListener('keydown', onPressKeyLandR);
+
+
+function onPressKeyLandR(e) {
+  if (e.key === 'ArrowLeft' && activeIndex > 0) {
+    activeIndex -= 1;
+    imgRef.src = galleryItems[activeIndex].original;
+  }
+}
+
 
